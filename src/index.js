99f908611cf73c006f1543f08fcde5aead8a7e14
Anushervon Abdusamadov <anushervon.a140@gmail.com>
2024-10-26 02:32:47 +0500

    add navbar module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const loadPartial = (elementId, partialFile) => {
-    fetch(partialFile)
-        .then(response => response.text())
-        .then(data => document.getElementById(elementId).innerHTML = data)
-        .catch(error => console.error('Ошибка загрузки partial:', error));
-}
+  fetch(partialFile)
+    .then((response) => response.text())
+    .then((data) => (document.getElementById(elementId).innerHTML = data))
+    .catch((error) => console.error("Ошибка загрузки partial:", error));
+};
 
-loadPartial('navbar', '../src/shared/modules/navbar/ui.html');
-loadPartial('order', '../src/pages/page-1/modules/order/ui.html');
-loadPartial('product-list', '../src/pages/page-1/modules/product-list/ui.html');
-
+loadPartial("navbar", "../src/shared/modules/navbar/ui.html");
+loadPartial("order", "../src/pages/page-1/modules/order/ui.html");
+loadPartial("main", "../src/pages/page-1/modules/main/ui.html");
+loadPartial("product-list", "../src/pages/page-1/modules/product-list/ui.html");
